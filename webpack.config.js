39c6ce6bb21ff00172ec60b6c1005f1f219d3bd5
AndriiPopov <andriy.popov.vl@gmail.com>
2020-07-11 00:11:55 +0200

    asdas

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const config = {
     // context: path.join(__dirname, 'src'),
-    entry: {
-        vendor: ['@babel/polyfill', 'react'], // Third party libraries
-        index: ['./src/Components/entrypoints/index.js'],
-        /// Every pages entry point should be mentioned here
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'), //destination for bundled output is under ./src/public
-        filename: '[name].js', // names of the bundled file will be name of the entry files (mentioned above)
-    },
+    // entry: {
+    //     vendor: ['@babel/polyfill', 'react'], // Third party libraries
+    //     index: ['./src/Components/entrypoints/index.js'],
+    //     /// Every pages entry point should be mentioned here
+    // },
+    // output: {
+    //     path: path.resolve(__dirname, 'public'), //destination for bundled output is under ./src/public
+    //     filename: '[name].js', // names of the bundled file will be name of the entry files (mentioned above)
+    // },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 use: {
-                    loader: 'babel-loader', // asks bundler to use babel loader to transpile es2015 code
+                    loader: "babel-loader", // asks bundler to use babel loader to transpile es2015 code
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
                     },
                 },
                 exclude: [/public/], ///node_modules/,
@@ -27,7 +27,7 @@ const config = {
         ],
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.json', '.wasm', '.mjs', '*'],
+        extensions: [".js", ".jsx", ".json", ".wasm", ".mjs", "*"],
     }, // If multiple files share the same name but have different extensions, webpack will resolve the one with the extension listed first in the array and skip the rest.
     // plugins: [
     //     new CopyWebpackPlugin([
@@ -37,6 +37,6 @@ const config = {
     //         },
     //     ]),
     // ],
-}
+};
 
-module.exports = config
+module.exports = config;
