35001fee314ec005656dfce8c180b2a4646a3b5b
AndriiPopov <andriy.popov.vl@gmail.com>
2020-07-11 00:44:12 +0200

    sdfds

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,49 +1,54 @@
-const authNotForce = require('../middleware/authNotForce')
+const authNotForce = require("../middleware/authNotForce");
 
-const { User } = require('../models/user')
-const { Account } = require('../models/account')
-const { Progress } = require('../models/progress')
-const express = require('express')
-const getAccount = require('../utils/getAccount')
+const { User } = require("../models/user");
+const { Account } = require("../models/account");
+const { Progress } = require("../models/progress");
+const express = require("express");
+const getAccount = require("../utils/getAccount");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/:_id*', authNotForce, async (req, res, next) => {
+router.get("/:_id*", authNotForce, async (req, res, next) => {
     try {
+        console.log("in profile");
         const profile = await Account.findById(req.params._id)
-            .select('name image friends goals progresses perks wallet')
-            .lean()
-        let account
+            .select("name image friends goals progresses perks wallet")
+            .lean();
+        let account;
+        console.log("in profile2");
+
         if (req.user) {
             account = await getAccount(
                 req,
                 res,
-                'name image friends wallet',
+                "name image friends wallet",
                 false,
                 true
-            )
+            );
         }
         if (!profile) {
             res.send({
                 account,
                 success: false,
-            })
-            return
+            });
+            return;
         }
+        console.log("in profile3");
 
         const progressesData = await Progress.find({
             _id: { $in: profile.progresses },
         })
             .lean()
-            .exec()
+            .exec();
 
-        let friends = profile.friends.map(item => item.friend)
+        let friends = profile.friends.map((item) => item.friend);
         friends = await Account.find({
             _id: { $in: friends },
         })
-            .select('name image')
+            .select("name image")
             .lean()
-            .exec()
+            .exec();
+        console.log("in profile4");
 
         res.send({
             account,
@@ -53,8 +58,8 @@ router.get('/:_id*', authNotForce, async (req, res, next) => {
                 progressesData,
             },
             success: true,
-        })
+        });
     } catch (ex) {}
-})
+});
 
-module.exports = router
+module.exports = router;
