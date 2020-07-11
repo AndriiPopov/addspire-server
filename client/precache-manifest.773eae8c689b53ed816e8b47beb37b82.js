d5879b51371387b9391b70a214ed223ed629c7ec
AndriiPopov <andriy.popov.vl@gmail.com>
2020-07-11 18:51:49 +0200

    sdfs

---
+++ client/precache-manifest.773eae8c689b53ed816e8b47beb37b82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5701738188ee7403fa06",
+    "url": "/static/js/main.b0c8827f.chunk.js"
+  },
+  {
+    "revision": "9910a8faddf0a1cf0a22",
+    "url": "/static/js/2.1c66c0bc.chunk.js"
+  },
+  {
+    "revision": "5701738188ee7403fa06",
+    "url": "/static/css/main.d94745f6.chunk.css"
+  },
+  {
+    "revision": "9910a8faddf0a1cf0a22",
+    "url": "/static/css/2.c9d71148.chunk.css"
+  },
+  {
+    "revision": "1593f3c17a2b1fe0c331390a964a0509",
+    "url": "/index.html"
+  }
+];
--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bc008b6a1e9742273e1fef92f679072.js"
+  "/precache-manifest.773eae8c689b53ed816e8b47beb37b82.js"
 );
 
 workbox.clientsClaim();
