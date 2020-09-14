2604f5544983c8120ec9bbb99b7ce979322eeed4
AndriiPopov <andriy.popov.vl@gmail.com>
2020-09-14 17:47:12 +0200

    sdas

--- client/precache-manifest.bab89141ee7eee91ee2f4599bb920931.js
+++ client/precache-manifest.bab89141ee7eee91ee2f4599bb920931.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "8015ac2e5e242a1296ac",
+    "revision": "66f04f0706be66e19df3",
     "url": "/static/js/main.fc9a0877.chunk.js"
   },
   {
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/static/js/2.aaedb659.chunk.js"
   },
   {
-    "revision": "8015ac2e5e242a1296ac",
-    "url": "/static/css/main.21d8cf71.chunk.css"
+    "revision": "66f04f0706be66e19df3",
+    "url": "/static/css/main.3111bf68.chunk.css"
   },
   {
     "revision": "9b666b5ce17f73cc4c1d",
     "url": "/static/css/2.f1d4a57b.chunk.css"
   },
   {
-    "revision": "6d8dd9b037d08fa104cf842c2694dd95",
+    "revision": "ddc3fddcb4c3ff3af28fbbf8947cfa1e",
     "url": "/index.html"
   }
 ];
--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6afb63aee551a50898e18b59f51c0577.js"
+  "/precache-manifest.bab89141ee7eee91ee2f4599bb920931.js"
 );
 
 workbox.clientsClaim();
