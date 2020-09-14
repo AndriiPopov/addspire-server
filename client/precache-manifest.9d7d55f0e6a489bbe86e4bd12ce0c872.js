2943b5b6b1cde74bea29e4f34494c52869991b33
AndriiPopov <andriy.popov.vl@gmail.com>
2020-09-14 22:24:11 +0200

    e5

---
+++ client/precache-manifest.9d7d55f0e6a489bbe86e4bd12ce0c872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6177c6d534bdf70a5ab",
+    "url": "/static/js/main.db3cdbc9.chunk.js"
+  },
+  {
+    "revision": "31753cdd691b86132844",
+    "url": "/static/js/2.7f1fc728.chunk.js"
+  },
+  {
+    "revision": "c6177c6d534bdf70a5ab",
+    "url": "/static/css/main.26502c90.chunk.css"
+  },
+  {
+    "revision": "31753cdd691b86132844",
+    "url": "/static/css/2.4cd7341b.chunk.css"
+  },
+  {
+    "revision": "bccce54814fd44821b1a213324587082",
+    "url": "/index.html"
+  }
+];
