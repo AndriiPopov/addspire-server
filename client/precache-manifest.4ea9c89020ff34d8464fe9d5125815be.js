063f65e01429277503a9b9cb6237bf152701359f
AndriiPopov <andriy.popov.vl@gmail.com>
2020-09-24 01:50:30 +0200

    sad

---
+++ client/precache-manifest.4ea9c89020ff34d8464fe9d5125815be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c862a02155970fc039bf",
+    "url": "/static/js/main.963cea0a.chunk.js"
+  },
+  {
+    "revision": "25280f6b4fcc5b397d42",
+    "url": "/static/js/2.0009ab9d.chunk.js"
+  },
+  {
+    "revision": "c862a02155970fc039bf",
+    "url": "/static/css/main.e32c83ca.chunk.css"
+  },
+  {
+    "revision": "25280f6b4fcc5b397d42",
+    "url": "/static/css/2.14eff9ad.chunk.css"
+  },
+  {
+    "revision": "493dd2849e7680922318be9917c0ee5a",
+    "url": "/index.html"
+  }
+];
