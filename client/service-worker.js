80ac92f4c74137ad4f4dcc1d4f7da821748df343
AndriiPopov <andriy.popov.vl@gmail.com>
2021-01-07 12:45:02 +0100

    Add image and text to mindmap

--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cea22372b487f1f18e90f667f5055b7.js"
+  "/precache-manifest.c46edd73fd78a692bbd8594c8922f674.js"
 );
 
 workbox.clientsClaim();
